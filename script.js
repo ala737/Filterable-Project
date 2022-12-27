ace2523b3d7e268298d7f94467ec507f0defeb02
ala nouri <alanouri591@gmail.com>
2022-12-27 04:29:20 -0800

    I completed all the jQuery and the CSS

--- script.js
+++ script.js
@@ -2,3 +2,18 @@
 $(document).on('click','.projects-filter li',function(){
     $(this).addClass('project-filter-active').siblings().removeClass('project-filter-active')
 });
+
+$(document).ready(function(){
+
+    $('.list').click(function(){
+       const value = $(this).attr('data-filter');
+       if (value == 'all'){
+        $('.project-box').show('1000');
+       }
+       else{
+        $('.project-box').not('.'+value).hide('1000');
+        $('.project-box').filter('.'+value).show('1000');
+       }
+    });
+
+});
